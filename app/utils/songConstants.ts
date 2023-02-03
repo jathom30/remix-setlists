c2e27fbeb9a91f92694042873f24278ed735649c
jathom30 <jathom30@gmail.com>
2023-02-03 08:25:34 -0500

    update tempo chart

--- app/utils/songConstants.ts
+++ app/utils/songConstants.ts
@@ -1,7 +1,10 @@
+import { getColor } from "./tailwindColors"
+
 export const keyLetters = ['Ab', 'A', 'A#', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#']
 export const majorMinorOptions = [
   { label: 'Major', value: false },
   { label: 'Minor', value: true },
 ]
 
-export const heatColors = ['#005f73', '#0a9396', '#94d2bd', '#e9d8a6', '#ee9b00', '#ca6702', '#bb3e03', '#ae2012']
+const tempoColors = ['info', 'accent', 'success', 'warning', 'error']
+export const heatColors = tempoColors.map(color => getColor(color))
