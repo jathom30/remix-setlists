50427bd8673a699d79b5eb13ecd85a0b68c7dfd4
Jeff Thomas <jathom30@gmail.com>
2023-02-16 14:53:20 -0500

    Setting up better testing (#52)
    * update smoke test - very basic
    
    * skip smoke test for now. start assorted tests
    
    * some utils tests
    
    * beef up utils tests
    
    * more tests
    
    * fix maxwidth on songs route

---
+++ app/utils/utils.test.ts
@@ -0,0 +1,125 @@
+import { capitalizeFirstLetter, generateRandomHex, contrastColor, passwordStrength, getPasswordError } from './assorted'
+import { buttonKind, buttonSize, badgeKind } from './buttonStyles'
+import { valueAsType, getFields } from './form'
+import { getSortFromParam, sortByLabel } from './params'
+
+// Assorted utils
+describe('Assorted utils', () => {
+  test('Capitalize first letter', () => {
+    expect(capitalizeFirstLetter('test')).toBe('Test')
+  })
+
+  test('Generate random hex', () => {
+    expect(generateRandomHex()).length(6)
+  })
+
+  test('Contrast color', () => {
+    expect(contrastColor('#000000')).toBe('#FFFFFF')
+    expect(contrastColor('#FFFFFF')).toBe('#000000')
+    expect(contrastColor('#FF2D00')).toBe('#FFFFFF')
+  })
+
+  test('Password strength', () => {
+    const badPassword = passwordStrength('short')
+    expect(badPassword.strength).toBe(1)
+    expect(badPassword.tests.minCharacters).toBe(false)
+    expect(badPassword.tests.includesLetters).toBe(true)
+    expect(badPassword.tests.includesNumbers).toBe(false)
+    expect(badPassword.tests.includesSpecialCharacters).toBe(false)
+
+    const goodPassword = passwordStrength('passw0rD!')
+    expect(goodPassword.strength).toBe(4)
+    expect(goodPassword.tests.minCharacters).toBe(true)
+    expect(goodPassword.tests.includesLetters).toBe(true)
+    expect(goodPassword.tests.includesNumbers).toBe(true)
+    expect(goodPassword.tests.includesSpecialCharacters).toBe(true)
+  })
+
+  test('Get password error', () => {
+    const shortPasswordTests = passwordStrength('short').tests
+    expect(getPasswordError(shortPasswordTests)).toBe('Password must be at least 8 characters')
+
+    const passwordWithoutNumbersAndSpecialCharsTests = passwordStrength('password').tests
+    expect(getPasswordError(passwordWithoutNumbersAndSpecialCharsTests)).toBe('Password must include at least 1 number and special character')
+
+    const passwordWithoutNumbersTests = passwordStrength('password!').tests
+    expect(getPasswordError(passwordWithoutNumbersTests)).toBe('Password must include at least 1 number')
+
+    const passwordWithoutSpecialCharsTests = passwordStrength('password1').tests
+    expect(getPasswordError(passwordWithoutSpecialCharsTests)).toBe('Password must include at least 1 special character')
+
+    const passwordWithoutLettersTests = passwordStrength('12345678!').tests
+    expect(getPasswordError(passwordWithoutLettersTests)).toBe('Password must include at least 1 letter')
+
+    const passwordWithoutErrorsTests = passwordStrength('passw0rd!').tests
+    expect(getPasswordError(passwordWithoutErrorsTests)).toBe(undefined)
+  })
+})
+
+// Button styles
+describe('Button styles', () => {
+  test('Button kind', () => {
+    expect(buttonKind('accent')).toBe('btn-accent')
+    expect(buttonKind('active')).toBe('btn-active')
+  })
+
+  test('Button size', () => {
+    expect(buttonSize('sm')).toBe('btn-sm')
+    expect(buttonSize('md')).toBe('btn-md')
+  })
+
+  test('Badge kind', () => {
+    expect(badgeKind('accent')).toBe('badge-accent')
+    expect(badgeKind('error')).toBe('badge-error')
+  })
+})
+
+// Form utils
+describe('Form utils', () => {
+  test('Value as type', () => {
+    const stringValue = valueAsType('string', 'test')
+    expect(stringValue).toBe('test')
+
+    const booleanValue = valueAsType('boolean', 'true')
+    expect(booleanValue).toBe(true)
+
+    const numberValue = valueAsType('number', '42')
+    expect(numberValue).toBe(42)
+  })
+
+  test('Get fields', () => {
+    const formData = new FormData()
+    formData.set('name', 'test name')
+
+    const { fields, errors } = getFields<{ name: string; age: number }>(formData, [
+      { name: 'name', type: 'string', isRequired: true },
+      { name: 'age', type: 'number', isRequired: true },
+    ])
+
+    expect(fields.name).toBe('test name')
+    expect(errors.age).toBe('age is required')
+    expect(errors.name).toBe(undefined)
+  })
+})
+
+// Param utils
+describe('Param utils', () => {
+  test('Get sort from param', () => {
+    const ageSort = getSortFromParam('age:asc')
+    expect(ageSort).toStrictEqual({ age: 'asc' })
+    expect(getSortFromParam('')).toStrictEqual({ "": undefined })
+  })
+
+  test('Sort by label', () => {
+    const searchParams = new URLSearchParams()
+
+    searchParams.set('sort', 'name:asc')
+    expect(sortByLabel(searchParams)).toBe('Name A-Z')
+
+    searchParams.set('sort', 'name:desc')
+    expect(sortByLabel(searchParams)).toBe('Name Z-A')
+
+    searchParams.set('sort', 'tempo:asc')
+    expect(sortByLabel(searchParams)).toBe('Tempo slow-fast')
+  })
+})
--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,14 +1,15 @@
 import { faker } from "@faker-js/faker";
 
-describe.skip("smoke tests", () => {
+describe("smoke tests", () => {
   afterEach(() => {
     cy.cleanupUser();
   });
 
-  it("should allow you to register and login", () => {
+  it("should allow you to register", () => {
     const loginForm = {
+      name: faker.name.firstName('male'),
       email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
+      password: 'passW04d!',
     };
 
     cy.then(() => ({ email: loginForm.email })).as("user");
@@ -17,35 +18,21 @@ describe.skip("smoke tests", () => {
 
     cy.findByRole("link", { name: /sign up/i }).click();
 
+    cy.findByRole("textbox", { name: /name/i }).type(loginForm.name);
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
     cy.findByLabelText(/password/i).type(loginForm.password);
     cy.findByRole("button", { name: /create account/i }).click();
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
+    cy.findByText(/verification sent/i)
   });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
-    cy.login();
+  it.only("should allow you to create a band", () => {
+    cy.login()
+    cy.visitAndCheck('/home')
+    cy.findByRole('link', { name: /create new band/i }).click()
 
-    cy.visitAndCheck("/");
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
-
-    cy.findByRole("link", { name: /\+ new note/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText("No notes yet");
-  });
+    cy.findByText("Create a new band")
+    // cy.findByRole('textbox', { name: "name" }).type('My test band')
+    // cy.findByRole('button', { name: /create/i }).click()
+  })
 });
