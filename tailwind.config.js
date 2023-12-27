3884cbd811a2b8b8c9372cfdc5c630565ea46bb3
Jeff Thomas <jeffthomas@Jeffs-MBP.attlocal.net>
2023-12-26 20:40:05 -0500

    upgrade remix packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,4 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  // daisyUI config (optional)
-  daisyui: {
-    styled: true,
-    // themes: ["light", "dark"],
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
 };
