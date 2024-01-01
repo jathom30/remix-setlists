ef8ec9b3cae4afbdf22ca487df309b1c68a6086b
Jeff Thomas <jeffthomas@Jeffs-MBP.attlocal.net>
2023-12-31 19:47:23 -0500

    fix lint, update getcolor func to have better type safety

--- app/utils/tailwindColors.ts
+++ app/utils/tailwindColors.ts
@@ -4,10 +4,25 @@ import tailwindConfig from "../../tailwind.config.js";
 
 const fullConfig = resolveConfig(tailwindConfig);
 
-const colors = fullConfig.theme?.colors as unknown as Record<
-  string,
-  ({ opacityValue }: { opacityValue: number }) => string
->;
-
-export const getColor = (name: string, opactity?: number) =>
-  colors?.[name]({ opacityValue: opactity ?? 1 });
+const theme = fullConfig.theme;
+export const getColor = (name: string, opactity?: number): string => {
+  if (!theme) return "black";
+  if (!("colors" in theme)) return "black";
+  const colors = theme.colors as Record<
+    string,
+    | string
+    | Record<number, string>
+    | (({ opacityValue }: { opacityValue: number }) => string)
+  >;
+  if (!(name in colors)) return "black";
+  const color = colors[name];
+  if (typeof color === "string") return color;
+  if (typeof color === "function")
+    return color({ opacityValue: opactity ?? 1 });
+  if (typeof color === "object") {
+    if (!opactity) return color[500];
+    if (!(opactity in color)) return "black";
+    return color[opactity];
+  }
+  return "black";
+};
