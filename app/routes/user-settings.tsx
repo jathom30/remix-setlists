e080b9857c3607bf8a960e290ca66b29320563e8
Jeff Thomas <jathom30@gmail.com>
2024-06-17 13:30:43 -0400

    start forms for add bands

--- app/routes/user-settings.tsx
+++ app/routes/user-settings.tsx
@@ -1,11 +1,16 @@
 import { faPlusCircle, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Link, useFetcher, useLoaderData } from "@remix-run/react";
+import {
+  Link,
+  useFetcher,
+  useLoaderData,
+  json,
+  redirect,
+} from "@remix-run/react";
 import {
   ActionFunctionArgs,
   LoaderFunctionArgs,
-  json,
 } from "@remix-run/server-runtime";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -20,6 +25,15 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -42,12 +56,21 @@ import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { FlexList, MaxWidth } from "~/components";
 import { H1, H4, P } from "~/components/typography";
+import { createBand, updateBandByCode } from "~/models/band.server";
 import {
   getUserWithBands,
   updateUser,
   updateUserPassword,
 } from "~/models/user.server";
+import { removeMemberFromBand } from "~/models/usersInBands.server";
 import { requireUserId } from "~/session.server";
+
+const IntentEnums = z.enum([
+  "user-details",
+  "user-password",
+  "leave-band",
+  "new-band",
+]);
 
 const UserDetailsSchema = z
   .object({
@@ -73,6 +96,13 @@ const UserPasswordSchema = z
     }
   });
 
+const RemoveMemberFromBandSchema = z
+  .object({
+    band_id: z.string(),
+    intent: z.literal("leave-band"),
+  })
+  .required();
+
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = await getUserWithBands(request);
 
@@ -89,9 +119,13 @@ export async function action({ request }: ActionFunctionArgs) {
   const email = formData.get("user_email");
   const password = formData.get("password");
   const passwordConfirm = formData.get("password_confirm");
+  const bandId = formData.get("band_id");
+  const bandName = formData.get("band_name");
+  const bandCode = formData.get("band_code");
+  const isNewBand = formData.get("is_new") === "true";
   const intent = formData.get("intent");
 
-  if (intent === "user-details") {
+  if (intent === IntentEnums.Enum["user-details"]) {
     const parsedData = UserDetailsSchema.parse({
       user_name: userName,
       user_email: email,
@@ -105,7 +139,7 @@ export async function action({ request }: ActionFunctionArgs) {
     return json({ success: true });
   }
 
-  if (intent === "user-password") {
+  if (intent === IntentEnums.Enum["user-password"]) {
     const parsedData = UserPasswordSchema.parse({
       password: password,
       password_confirm: passwordConfirm,
@@ -115,6 +149,31 @@ export async function action({ request }: ActionFunctionArgs) {
     await updateUserPassword(userId, parsedData.password);
     return json({ success: true });
   }
+
+  if (intent === IntentEnums.Enum["leave-band"]) {
+    const parsedData = RemoveMemberFromBandSchema.parse({
+      band_id: bandId,
+      intent,
+    });
+    await removeMemberFromBand(parsedData.band_id, userId);
+    return json({ success: true });
+  }
+
+  if (intent === IntentEnums.Enum["new-band"]) {
+    if (isNewBand) {
+      const parsedData = NewBandSchema.parse({ band_name: bandName });
+      const band = await createBand({ name: parsedData.band_name }, userId);
+      return redirect(`/${band.id}/setlists`);
+    } else {
+      const parsedData = AddBandSchema.parse({ band_code: bandCode });
+      const band = await updateBandByCode(parsedData.band_code, userId);
+      if ("error" in band) {
+        return json(band);
+      }
+      return redirect(`/${band.id}/setlists`);
+    }
+  }
+
   return null;
 }
 
@@ -127,11 +186,13 @@ export default function UserSettings() {
     defaultValues: {
       user_name: user.name || "",
       user_email: user.email,
-      intent: "user-details",
+      intent: IntentEnums.Enum["user-details"],
     },
   });
 
-  const userDetailsFetcher = useFetcher({ key: "user-details" });
+  const userDetailsFetcher = useFetcher({
+    key: IntentEnums.Enum["user-details"],
+  });
 
   const onUserSettingsSubmit = (data: z.infer<typeof UserDetailsSchema>) => {
     userDetailsFetcher.submit(data, { method: "put" });
@@ -143,11 +204,13 @@ export default function UserSettings() {
     defaultValues: {
       password: "",
       password_confirm: "",
-      intent: "user-password",
+      intent: IntentEnums.Enum["user-password"],
     },
   });
 
-  const userPasswordFetcher = useFetcher({ key: "user-password" });
+  const userPasswordFetcher = useFetcher({
+    key: IntentEnums.Enum["user-password"],
+  });
 
   const onUserPasswordSubmit = (data: z.infer<typeof UserPasswordSchema>) => {
     userPasswordFetcher.submit(data, { method: "put" });
@@ -265,7 +328,7 @@ export default function UserSettings() {
                   hidden
                   type="hidden"
                   name="intent"
-                  defaultValue="user-details"
+                  defaultValue={IntentEnums.Enum["user-details"]}
                 />
                 <Button variant="ghost" onClick={() => userDetailsForm.reset()}>
                   Cancel
@@ -335,7 +398,7 @@ export default function UserSettings() {
                   hidden
                   type="hidden"
                   name="intent"
-                  defaultValue="user-password"
+                  defaultValue={IntentEnums.Enum["user-password"]}
                 />
                 <Button
                   variant="ghost"
@@ -354,10 +417,7 @@ export default function UserSettings() {
           <CardHeader>
             <FlexList direction="row" items="center" justify="between" gap={2}>
               <H4>Associated Bands</H4>
-              <Button variant="outline">
-                <FontAwesomeIcon icon={faPlusCircle} className="mr-2" />
-                Add Band
-              </Button>
+              <AddBand />
             </FlexList>
             <CardDescription>
               You can be associated with any number of bands.
@@ -374,11 +434,17 @@ export default function UserSettings() {
                     {band.role === "ADMIN" ? (
                       <Button variant="ghost">Edit</Button>
                     ) : null}
-                    <Button variant="secondary">Remove</Button>
+                    <RemoveSelfFromBand band={band} />
                   </FlexList>
                 </CardHeader>
               </Card>
             ))}
+            {user.bands.length === 0 ? (
+              <P>
+                You are not associated with any bands. Click "Add Band" to join
+                one.
+              </P>
+            ) : null}
           </CardContent>
         </Card>
 
@@ -421,3 +487,187 @@ export default function UserSettings() {
     </div>
   );
 }
+
+const BandFormSchema = z
+  .object({
+    is_new: z.boolean(),
+    band_name: z.string(),
+    band_code: z.string(),
+    intent: z.literal(IntentEnums.Enum["new-band"]),
+  })
+  .superRefine((data) => {
+    if (data.is_new) {
+      NewBandSchema.parse({ band_name: data.band_name });
+    } else {
+      AddBandSchema.parse({ band_code: data.band_code });
+    }
+  });
+
+const NewBandSchema = z
+  .object({
+    band_name: z.string().min(1),
+  })
+  .required();
+
+const AddBandSchema = z
+  .object({
+    band_code: z.coerce
+      .string()
+      .length(5, "Band code must be exactly 5 characters."),
+  })
+  .required();
+
+const AddBand = () => {
+  const fetcher = useFetcher<typeof action>({
+    key: IntentEnums.Enum["new-band"],
+  });
+
+  const form = useForm({
+    resolver: zodResolver(BandFormSchema),
+    defaultValues: {
+      is_new: false,
+      band_name: "",
+      band_code: "",
+      intent: IntentEnums.Enum["new-band"],
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof BandFormSchema>) => {
+    fetcher.submit(data, { method: "put" });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <FontAwesomeIcon icon={faPlusCircle} className="mr-2" />
+          Add Band
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Band</DialogTitle>
+          <DialogDescription>
+            You can either create a new band or join an existing one.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="is_new"
+              render={({ field }) => (
+                <FlexList direction="row" items="center" gap={2} pad={{ b: 2 }}>
+                  <Switch
+                    id="band-toggle"
+                    checked={field.value}
+                    onCheckedChange={(checked) => {
+                      field.onChange(checked);
+                      if (checked) {
+                        form.setValue("band_code", "");
+                      } else {
+                        form.setValue("band_name", "");
+                      }
+                    }}
+                  />
+                  <Label htmlFor="band-toggle">Create New Band</Label>
+                </FlexList>
+              )}
+            />
+            {form.watch("is_new") ? (
+              <FormField
+                control={form.control}
+                name="band_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Band Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Band Name"
+                        {...field}
+                        disabled={!form.watch("is_new")}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Create your new band here.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ) : (
+              <FormField
+                control={form.control}
+                name="band_code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Band Code</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Band Code"
+                        {...field}
+                        disabled={form.watch("is_new")}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Ask your band's admin for the code to join.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            <input
+              hidden
+              type="hidden"
+              name="intent"
+              value={IntentEnums.Enum["new-band"]}
+            />
+            <DialogFooter className="pt-4">
+              <Button type="submit">
+                {form.watch("is_new") ? "Create Band" : "Add Band"}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const RemoveSelfFromBand = ({
+  band,
+}: {
+  band: { userId: string; bandName: string; bandId: string; role: string };
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="secondary">Remove</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Leave {band.bandName}?</DialogTitle>
+          <DialogDescription>
+            You can leave this band at any time, but you will no longer have
+            access to any of its songs and setlists.
+          </DialogDescription>
+        </DialogHeader>
+        <form method="post">
+          <input hidden type="hidden" name="band_id" value={band.bandId} />
+          <input
+            hidden
+            type="hidden"
+            name="intent"
+            value={IntentEnums.Enum["leave-band"]}
+          />
+          <DialogFooter>
+            <Button type="submit" variant="destructive">
+              Leave Band
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
