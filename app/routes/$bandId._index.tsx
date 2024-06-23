2227f6d5c006fb7a4e016b58d7c8e657f3b6ced5
Jeff Thomas <jathom30@gmail.com>
2024-06-23 15:01:22 -0400

    add feels to dashboard

--- app/routes/$bandId._index.tsx
+++ app/routes/$bandId._index.tsx
@@ -14,10 +14,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { FlexList } from "~/components";
+import { FeelContainer } from "~/components/feel-container";
 import { SetlistContainer } from "~/components/setlist-container";
 import { SongContainer } from "~/components/song-container";
 import { H1, Small, P } from "~/components/typography";
 import { getBand } from "~/models/band.server";
+import { getMostRecentFeels } from "~/models/feel.server";
 import { getRecentSetlists } from "~/models/setlist.server";
 import { getRecentSongs } from "~/models/song.server";
 import { requireUserId } from "~/session.server";
@@ -33,7 +35,8 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   }
   const setlists = await getRecentSetlists(bandId);
   const songs = await getRecentSongs(bandId);
-  return json({ setlists, songs, band });
+  const feels = await getMostRecentFeels(bandId);
+  return json({ setlists, songs, band, feels });
 }
 
 const useRedirectHome = () => {
@@ -48,7 +51,7 @@ const useRedirectHome = () => {
 
 export default function BandId() {
   useRedirectHome();
-  const { setlists, songs, band } = useLoaderData<typeof loader>();
+  const { setlists, songs, band, feels } = useLoaderData<typeof loader>();
   const user = useUser();
   return (
     <div className="p-2 space-y-2">
@@ -70,91 +73,127 @@ export default function BandId() {
           </Small>
         </CardContent>
       </Card>
-      <Card>
-        <CardHeader>
-          <FlexList direction="row" items="center" justify="between">
-            <CardTitle>Recent Setlists</CardTitle>
-            {setlists.length ? (
-              <Button variant="outline" asChild>
-                <Link to="setlists">See all</Link>
-              </Button>
+      <div className="grid gap-2 md:grid-cols-2">
+        <Card>
+          <CardHeader>
+            <FlexList direction="row" items="center" justify="between">
+              <CardTitle>Recent Setlists</CardTitle>
+              {setlists.length ? (
+                <Button variant="outline" asChild>
+                  <Link to="setlists">See all</Link>
+                </Button>
+              ) : null}
+            </FlexList>
+            <CardDescription>
+              Here are the most recently updated setlists
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FlexList gap={1}>
+              {setlists.map((setlist) => (
+                <Link key={setlist.id} to={`setlists/${setlist.id}`}>
+                  <SetlistContainer setlist={setlist} />
+                </Link>
+              ))}
+            </FlexList>
+            {setlists.length === 0 ? (
+              <div className="text-center">
+                <P>This band has no setlists yet.</P>
+                <Button asChild>
+                  <Link to="setlists/new">Create your first setlist here</Link>
+                </Button>
+              </div>
             ) : null}
-          </FlexList>
-          <CardDescription>
-            Here are the most recently updated setlists
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <FlexList gap={1}>
-            {setlists.map((setlist) => (
-              <Link key={setlist.id} to={`setlists/${setlist.id}`}>
-                <SetlistContainer setlist={setlist} />
-              </Link>
-            ))}
-          </FlexList>
-          {setlists.length === 0 ? (
-            <div className="text-center">
-              <P>This band has no setlists yet.</P>
-              <Button asChild>
-                <Link to="setlists/new">Create your first setlist here</Link>
-              </Button>
-            </div>
-          ) : null}
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <FlexList direction="row" items="center" justify="between">
-            <CardTitle>Recent Songs</CardTitle>
-            {songs.length ? (
-              <Button variant="outline" asChild>
-                <Link to="songs">See all</Link>
-              </Button>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <FlexList direction="row" items="center" justify="between">
+              <CardTitle>Recent Songs</CardTitle>
+              {songs.length ? (
+                <Button variant="outline" asChild>
+                  <Link to="songs">See all</Link>
+                </Button>
+              ) : null}
+            </FlexList>
+            <CardDescription>
+              Here are the most recently updated songs
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FlexList gap={1}>
+              {songs.map((song) => (
+                <Link key={song.id} to={`songs/${song.id}`}>
+                  <SongContainer.Card>
+                    <SongContainer.Song song={song} />
+                  </SongContainer.Card>
+                </Link>
+              ))}
+            </FlexList>
+            {songs.length === 0 ? (
+              <div className="text-center">
+                <P>This band has no songs yet.</P>
+                <Button asChild>
+                  <Link to="songs/new">Create your first song here</Link>
+                </Button>
+              </div>
             ) : null}
-          </FlexList>
-          <CardDescription>
-            Here are the most recently updated songs
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <FlexList gap={1}>
-            {songs.map((song) => (
-              <Link key={song.id} to={`songs/${song.id}`}>
-                <SongContainer.Card>
-                  <SongContainer.Song song={song} />
-                </SongContainer.Card>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <FlexList direction="row" items="center" justify="between">
+              <CardTitle>Recent Feels</CardTitle>
+              {songs.length ? (
+                <Button variant="outline" asChild>
+                  <Link to="feels">See all</Link>
+                </Button>
+              ) : null}
+            </FlexList>
+            <CardDescription>
+              Here are the most recently updated feels
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FlexList gap={1}>
+              {feels.map((feel) => (
+                <Link key={feel.id} to={`feels/${feel.id}`}>
+                  <FeelContainer.Card>
+                    <FeelContainer.Feel feel={feel} />
+                  </FeelContainer.Card>
+                </Link>
+              ))}
+            </FlexList>
+            {songs.length === 0 ? (
+              <div className="text-center">
+                <P>This band has no feels yet.</P>
+                <Button asChild>
+                  <Link to="feels/new">Create your first feel here</Link>
+                </Button>
+              </div>
+            ) : null}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardDescription>Additional Links</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2 sm:flex-row md:flex-col">
+            <Button className="w-full" variant="outline" asChild>
+              <Link to="band-settings">
+                <Settings className="h-4 w-4 mr-2" />
+                Band Settings
               </Link>
-            ))}
-          </FlexList>
-          {songs.length === 0 ? (
-            <div className="text-center">
-              <P>This band has no songs yet.</P>
-              <Button asChild>
-                <Link to="songs/new">Create your first song here</Link>
-              </Button>
-            </div>
-          ) : null}
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardDescription>Additional Links</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-2 sm:flex-row">
-          <Button className="w-full" variant="outline" asChild>
-            <Link to="band-settings">
-              <Settings className="h-4 w-4 mr-2" />
-              Band Settings
-            </Link>
-          </Button>
-          <Button className="w-full" variant="outline" asChild>
-            <Link to="/home">
-              <Boxes className="h-4 w-4 mr-2" />
-              Back to Band Selection
-            </Link>
-          </Button>
-        </CardContent>
-      </Card>
+            </Button>
+            <Button className="w-full" variant="outline" asChild>
+              <Link to="/home">
+                <Boxes className="h-4 w-4 mr-2" />
+                Back to Band Selection
+              </Link>
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
